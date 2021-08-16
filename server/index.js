44f61b625d80ace2655d524dcc75c6d36d15e36a
Sahil Kedare <sahilkedare4444@gmail.com>
2021-08-16 10:23:28 +0530

    like create and delete done

--- server/index.js
+++ server/index.js
@@ -13,7 +13,6 @@ app.use(express.urlencoded({limit: "30mb",extended: true}));
 app.use(cors());
 
 app.use('/posts', postRoutes);
-// const CONNECTION_URL = 'mongodb+srv://sahilkedare:sahilkedare123@cluster0.he2wa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 
 const PORT = process.env.PORT || 5000;
